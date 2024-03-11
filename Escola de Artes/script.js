833ecf2356900f49e974bd34bf2f21f3ba438949
Carla Mozena <128012862+Carla-coder@users.noreply.github.com>
2024-03-10 21:41:39 -0300

    Add files via upload

---
+++ Escola de Artes/script.js
@@ -0,0 +1,114 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var cursoModals = document.querySelectorAll('[data-bs-toggle="modal"]');
+
+  cursoModals.forEach(function (modal) {
+    modal.addEventListener('click', function () {
+      var targetModalId = modal.getAttribute('data-bs-target');
+      var targetModal = new bootstrap.Modal(document.querySelector(targetModalId));
+
+      // Esconde as imagens dos cards ao abrir o modal
+      var cardImages = document.querySelectorAll('.card-img-top');
+      cardImages.forEach(function (image) {
+        image.style.display = 'none';
+      });
+
+      targetModal.show();
+    });
+  });
+
+  var modals = document.querySelectorAll('.modal');
+  modals.forEach(function (modal) {
+    modal.addEventListener('hidden.bs.modal', function () {
+      // Restaura as imagens dos cards ao fechar o modal
+      var cardImages = document.querySelectorAll('.card-img-top');
+      cardImages.forEach(function (image) {
+        image.style.display = 'block';
+      });
+
+      // Remove o backdrop ao fechar o modal
+      var modalBackdrop = document.querySelector('.modal-backdrop');
+      if (modalBackdrop) {
+        modalBackdrop.parentNode.removeChild(modalBackdrop);
+      }
+
+      // Restaura o corpo da página ao estado normal
+      document.body.style.overflow = 'auto';
+    });
+  });
+});
+
+
+// Esta parte do código foi para fins de estudo //
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   // Modais dos cursos
+//   var cursoModals = document.querySelectorAll('[data-bs-toggle="modal"]')
+//   cursoModals.forEach(function (modal) {
+//     modal.addEventListener('click', function () {
+//       var targetModalId = modal.getAttribute('data-bs-target')
+//       var targetModal = new bootstrap.Modal(
+//         document.querySelector(targetModalId)
+//       )
+//       targetModal.show()
+//     })
+//   })
+
+//    // Adiciona evento ao fechar o modal
+//    var modals = document.querySelectorAll('.modal')
+//    modals.forEach(function (modal) {
+//      modal.addEventListener('hidden.bs.modal', function () {
+//        document.body.classList.remove('modal-open')
+//        var modalBackdrops = document.getElementsByClassName('modal-backdrop')
+//        for (var i = 0; i < modalBackdrops.length; i++) {
+//        modalBackdrops[i].parentNode.removeChild(modalBackdrops[i])
+//       }
+//      })
+//    })
+
+  // // Adiciona evento ao formulário de matrícula
+  // var matriculaForm = document.querySelector('#matricula-form')
+  // if (matriculaForm) {
+  //   matriculaForm.addEventListener('submit', function (event) {
+  //     event.preventDefault()
+  //     processarMatriculaForm()
+  //   })
+  // }
+
+//   // Função para processar o formulário de matrícula
+//   function processarMatriculaForm () {
+//     // Lógica para processar o formulário de matrícula
+//     console.log('Formulário de matrícula enviado com sucesso!')
+//     exibirMensagemDeSucesso(
+//       'Formulário de matrícula enviado com sucesso!',
+//       'success'
+//     )
+//   }
+
+//   // Adiciona evento ao formulário de contato
+//   var contatoForm = document.querySelector('#contato-form')
+//   if (contatoForm) {
+//     contatoForm.addEventListener('submit', function (event) {
+//       event.preventDefault()
+//       processarContatoForm()
+//     })
+//   }
+
+//   // Função para processar o formulário de contato
+//   function processarContatoForm () {
+//     // Lógica para processar o formulário de contato
+//     console.log('Formulário de contato enviado com sucesso!')
+//     exibirMensagemDeSucesso(
+//       'Formulário de contato enviado com sucesso!',
+//       'success'
+//     )
+//   }
+
+//   // Função para exibir mensagem na página
+//   function exibirMensagemDeSucesso (mensagem, tipo) {
+//     var mensagensDiv = document.getElementById('mensagens')
+//     var mensagemElement = document.createElement('div')
+//     mensagemElement.className = 'alert alert-' + tipo
+//     mensagemElement.textContent = mensagem
+//     mensagensDiv.appendChild(mensagemElement)
+//   }
+ 
